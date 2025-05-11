e7dd18d136d32b1429edb8dbd694648877f95d36
Luis Henrique <42474086+henriqueannicchino@users.noreply.github.com>
2025-05-11 14:53:01 +0200

    up

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,11 @@
-const express = require('express');
+const express = require("express");
 const routes = express.Router();
 
-routes.get('/hello',(req, res) => {
-    res.send('Hello World');
+routes.get("/hello", (req, res) => {
+  res.send({ message: "Hello World" });
 });
 
-const TelegramBotController = require('./controllers/TelegramBotController');
-routes.post('/sendLocation', TelegramBotController.store);
+const TelegramBotController = require("./controllers/TelegramBotController");
+routes.post("/sendLocation", TelegramBotController.store);
 
 module.exports = routes;
